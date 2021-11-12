a2cca9f505448ceb4dc4137b438d1b34dd79fb4b
Yishayahu <ishayahu156@gmail.com>
2021-11-12 14:04:19 +0200

    f

--- spottunet/torch/fine_tune_policy.py
+++ spottunet/torch/fine_tune_policy.py
@@ -12,31 +12,38 @@ class FineTunePolicy(Policy):
     def __init__(self,architecture,optimizer):
         self.optimizer = optimizer
         self.layers = [x for x in architecture.named_modules() if isinstance(x[1], nn.Conv2d) or isinstance(x[1], nn.BatchNorm2d) or isinstance(x[1],nn.ConvTranspose2d)]
-        self.unfreezed_layers = {0}
-        self.optimizer.param_groups[0]['params'] = list(self.layers[0][1].parameters())
+        self.unfreezed_layers = {}
+        self.optimizer.param_groups[0]['params'] = []
         self.grad_per_layer = torch.zeros(len(self.layers))
         self.last_best = [0,0]
         for layer_index,(n1,m1) in enumerate(self.layers):
+            if 'init_path.0' in n1 or 'init_path.1' in n1:
+                self.unfreezed_layers[layer_index] = n1
+                self.optimizer.param_groups[0]['params'].extend(list(m1.parameters()))
             m1.register_full_backward_hook(self.collect_grads(layer_index))
-
+        print(f'current unfreeze {self.unfreezed_layers}')
 
     def epoch_started(self, epoch: int):
         self.grad_per_layer = torch.zeros(len(self.layers))
     def epoch_finished(self, epoch: int, train_losses: Sequence, metrics: dict = None, policies: dict = None):
         if self.detect_plateau(metrics):
-            layer_to_unfreeze_name,layer_to_unfreeze = self.layers[torch.argmax(self.grad_per_layer)]
-            print(f'unfreezing {layer_to_unfreeze_name} current state is {layer_to_unfreeze.weight.requires_grad}')
+            layer_index = torch.argmax(self.grad_per_layer)
+            layer_to_unfreeze_name,layer_to_unfreeze = self.layers[layer_index]
+            print(f'unfreezing {layer_to_unfreeze_name}')
+            self.unfreezed_layers[layer_index] = layer_to_unfreeze_name
+            print(f'current unfreeze {self.unfreezed_layers}')
             self.optimizer.param_groups[0]['params'].extend(list(layer_to_unfreeze.parameters()))
 
 
     def detect_plateau(self,metrics):
         curr_metric = metrics['sdice_score']
-
-        if curr_metric < self.last_best[1] < self.last_best[0]:
+        if curr_metric < self.last_best[1]*1.001 and self.last_best[1]< self.last_best[0]*1.001:
             to_ret = True
         else:
             to_ret = False
         self.last_best.append(curr_metric)
+        print('last scores')
+        print(self.last_best)
         self.last_best = self.last_best[-2:]
         return to_ret
 
