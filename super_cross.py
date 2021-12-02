6fa8dcd2fed4146e981e63cee0f11f3ad51beb6c
Yishayahu <ishayahu156@gmail.com>
2021-12-02 14:26:11 +0200

    super cross

--- super_cross.py
+++ super_cross.py
@@ -1,6 +1,7 @@
 import json
 import multiprocessing
 import os
+import subprocess
 import sys
 import time
 from itertools import combinations
@@ -18,7 +19,7 @@ def find_available_device(my_devices):
         wanted_free_mem = 16 * 2 ** 30  # at least 16 GB avail
         while True:
             for device_num in range(nvmlDeviceGetCount()):
-                if device_num in my_devices:
+                if f'cuda:{device_num}' in my_devices:
                     continue
                 h = nvmlDeviceGetHandleByIndex(device_num)
                 info = nvmlDeviceGetMemoryInfo(h)
@@ -30,14 +31,17 @@ def find_available_device(my_devices):
     else:
         return 'cpu'
 
-def run_single_exp(exp,device,source,target,ts,sdice_path,stats,my_devices):
+def run_single_exp(exp,device,source,target,ts,sdice_path,my_devices,ret_value):
     my_devices.append(device)
     print(f'training on source {source} target {target} exp {exp} on device {device} my devices is {my_devices}')
-    sys.stdout = open(f'{exp}_logs_out.txt', 'w')
-    sys.stderr = open(f'{exp}_logs_err.txt', 'w')
-    os.system(f'python trainer.py --config {exp} --exp_name {exp} --device {device} --source {source} --target {target} --ts_size {ts}')
-    sdice = np.mean(list(json.load(open(sdice_path)).values()))
-    stats[exp][ts][f's_{source} t_{target}'] = sdice
+    try:
+        subprocess.run(f'python trainer.py --config {exp} --exp_name {exp} --device {device} --source {source} --target {target} --ts_size {ts} >  errs_and_outs/{exp}_logs_out.txt 2> errs_and_outs/{exp}_logs_err.txt',shell=True,check=True)
+        sdice = np.mean(list(json.load(open(sdice_path)).values()))
+        ret_value.value = sdice
+    except subprocess.CalledProcessError:
+        print(f'error in exp {exp}')
+
+
     my_devices.remove(device)
 def main():
     if torch.cuda.is_available():
@@ -48,7 +52,8 @@ def main():
     experiments = adam_exps+sgd_exps
     target_sizes = [2]#[1,2,4,8]
     combs = list(combinations(list(range(6)), 2))
-    stats = manager.dict()
+    stats = {}
+
     running_now = []
     my_devices = manager.list()
     for combination in tqdm(combs):
@@ -56,8 +61,9 @@ def main():
             for exp in experiments:
                 if exp not in stats:
                     stats[exp] = {}
-                    if ts not in stats[exp]:
-                        stats[exp][ts] = {}
+
+                if ts not in stats[exp]:
+                    stats[exp][ts] = {}
                 source, target = combination
                 if source == 0 and target == 2:
                     continue
@@ -78,15 +84,17 @@ def main():
                 if not os.path.exists(sdice_path):
                     curr_device = find_available_device(my_devices)
                     print(f'lunch on source {source} target {target} exp {exp}')
-
-                    p = Process(target=run_single_exp,args=(exp,curr_device,source,target,ts,sdice_path,stats,my_devices))
-                    running_now.append(p)
+                    ret_value = multiprocessing.Value("d", 0.0, lock=False)
+                    p = Process(target=run_single_exp,args=(exp,curr_device,source,target,ts,sdice_path,my_devices,ret_value))
+                    running_now.append([(exp,ts,f's_{source} t_{target}'),p,ret_value])
                     p.start()
+                    time.sleep(5)
                 else:
                     sdice = np.mean(list(json.load(open(sdice_path)).values()))
                     stats[exp][ts][f's_{source} t_{target}'] = sdice
-    for p in running_now:
+    for place,p,ret_value in tqdm(running_now,desc='finishing running now'):
         p.join()
+        stats[place[0]][place[1]][place[2]] = ret_value.value
     print(stats)
     json.dump(stats,open('all_stats.json','w'))
 
