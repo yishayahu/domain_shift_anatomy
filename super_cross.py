48fad7e457569a1b65f07a551dbf01c101acb459
Yishayahu <ishayahu156@gmail.com>
2021-12-02 13:30:45 +0200

    super cross

--- super_cross.py
+++ super_cross.py
@@ -7,31 +7,36 @@ from itertools import combinations
 import numpy as np
 from multiprocessing import Process
 
+import torch.cuda
 from tqdm import tqdm
 from wandb.vendor.pynvml.pynvml import nvmlDeviceGetCount, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, \
     nvmlDeviceGetUtilizationRates, nvmlInit
 
 
 def find_available_device():
-    wanted_free_mem = 16 * 2 ** 30  # at least 16 GB avail
-    while True:
-        for device_num in range(nvmlDeviceGetCount()):
-            h = nvmlDeviceGetHandleByIndex(device_num)
-            info = nvmlDeviceGetMemoryInfo(h)
-            gpu_utilize = nvmlDeviceGetUtilizationRates(h)
-            if info.free > wanted_free_mem and gpu_utilize.gpu < 1:
-                return device_num
-        time.sleep(60)
-        print('looking for device')
+    if torch.cuda.is_available():
+        wanted_free_mem = 16 * 2 ** 30  # at least 16 GB avail
+        while True:
+            for device_num in range(nvmlDeviceGetCount()):
+                h = nvmlDeviceGetHandleByIndex(device_num)
+                info = nvmlDeviceGetMemoryInfo(h)
+                gpu_utilize = nvmlDeviceGetUtilizationRates(h)
+                if info.free > wanted_free_mem and gpu_utilize.gpu < 1:
+                    return f'cuda:{device_num}'
+            time.sleep(60)
+            print('looking for device')
+    else:
+        return 'cpu'
 
 def run_single_exp(exp,device,source,target,ts,sdice_path,stats):
     print(f'training on source {source} target {target} exp {exp}')
-    sys.stdout = open(os.devnull, 'w')
+    sys.stdout = open(f'{exp}_logs.txt', 'w')
     os.system(f'python trainer.py --config {exp} --exp_name {exp} --device {device} --source {source} --target {target} --ts_size {ts}')
     sdice = np.mean(list(json.load(open(sdice_path)).values()))
     stats[exp][ts][f's_{source} t_{target}'] = sdice
 def main():
-    nvmlInit()
+    if torch.cuda.is_available():
+        nvmlInit()
     manager = multiprocessing.Manager()
     sgd_exps = ['posttrain','gradual_tl','spottune','posttrain_continue_optimizer']
     adam_exps = ['posttrain_adam', 'gradual_tl_adam', 'spottune_adam', 'posttrain_continue_optimizer_adam','spot_with_grad_adam']
@@ -54,7 +59,7 @@ def main():
                 src_ckpt_path = f'/home/dsi/shaya/data_splits/sources/source_{source}/model_{adam_or_sgd}.pth'
 
                 if not os.path.exists(src_ckpt_path):
-                    print(f'training on source {source}')
+                    print(f'training on source {source} to create {src_ckpt_path}')
                     os.system(f'python trainer.py --config only_source_{adam_or_sgd} --exp_name only_source_{adam_or_sgd} --device {find_available_device()} --source {source} --train_only_source')
                     pp_model = f'/home/dsi/shaya/spottune_results/source_{source}/only_source_{adam_or_sgd}/checkpoints/checkpoint_59/model.pth'
                     os.rename(pp_model,src_ckpt_path)
